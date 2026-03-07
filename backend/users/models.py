22967eb16ad7e574e9783957e347fa2d72a76da8
kjohnbosco62-a11y <kjohnbosco62@gmail.com>
2026-03-07 11:35:19 +0300

    update models file

---
+++ backend/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Issue(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+class customUser(AbstractUser):
+    
+    ROLE_CHOICES = (
+        ("student","student"),
+        ()
+    )
