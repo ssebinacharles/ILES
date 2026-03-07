f846979315b3d5e8453f3b3f3bb03fe6b4b29eb0
kjohnbosco62-a11y <kjohnbosco62@gmail.com>
2026-03-07 12:10:34 +0300

    Enhance customUser model with additional fields
    Added fields for role, department, staff number, student number, organization, internship dates, and profile picture to customUser model.

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,8 +8,47 @@ class Issue(models.Model):
     def __str__(self):
         return self.title
 class customUser(AbstractUser):
-    
     ROLE_CHOICES = (
         ("student","student"),
-        ()
+        ("workplace",workplace supervisor),
+         ("Academic","Academic Supervisor"),
+         ("admin"," Adminstrator")
+    
     )
+role = models.Charfield(
+    max_length=30,
+    choices=ROLE_CHOICES
+)
+department = models.charField(
+    max_length=200,
+    blank=True,
+    null=True
+)
+staff_number = models.CharField(
+    max_length=40,
+    unique=True,
+    blank=True,
+    null=True
+)
+student_number =  models.CharFeild(
+    max_lenth=40,
+    unique=True,
+    blank=True,
+    null=True
+)
+organisation = models.CharField(
+    max_lenth=200,
+    blank=True,
+    null=True
+)
+internship_start = models.DateField(
+    blank=True,
+    null=True
+)
+internship_end = models.DateFeild(
+    blank=True,
+    null=True
+)
+profile_picture = models.lmageFeild(upload_to"profiles/" , blank=true, null=True)
+def __str__(self):
+    return self.username
