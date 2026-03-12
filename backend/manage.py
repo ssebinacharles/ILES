d3f7fd197a9259e0984f49c36f5c4e7be68b5a04
ssebinacharles <ssebinacharles50@gmail.com>
2026-03-12 20:03:13 +0300

    Revert "Resolve stash conflict for models.py"

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iles_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aits_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
