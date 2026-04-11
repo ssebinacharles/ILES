41659cbd07103360091742694d26a7c3793a60ce
Olietho Godfrey Josiah <jj.o.godfrey@gmail.com>
2026-04-11 09:28:27 +0300

    Update settings.py

--- backend/iles_backend/settings.py
+++ backend/iles_backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simple_history'
+    'simple_history',
     
     'rest_framework',
     'users',
