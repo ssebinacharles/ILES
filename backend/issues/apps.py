23ad0950e09912b4a4c25a78b280a54ae982dcf8
ssebinacharles <ssebinacharles50@gmail.com>
2026-04-11 13:36:31 +0000

    Update apps.py

--- backend/issues/apps.py
+++ backend/issues/apps.py
@@ -2,4 +2,14 @@ from django.apps import AppConfig
 
 
 class IssuesConfig(AppConfig):
-    name = 'issues'
+
+    # Use BigAutoField for automatic primary key fields by default.
+    default_auto_field = "django.db.models.BigAutoField"
+    
+    name = "issues"
+    # Provide a human readable verbose name for the admin interface.
+    verbose_name = "Issues"
+
+    def ready(self) -> None:
+        
+        from . import signals  
