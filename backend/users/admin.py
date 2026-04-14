9d9807cf005c7972ed3dd33bf09adc53a47b3464
kjohnbosco62-a11y <kjohnbosco62@gmail.com>
2026-04-14 12:38:28 +0000

    admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from __future__ import annotations
 
+from typing import Any
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import (
+    User,
+    StudentProfile,
+    SupervisorProfile,
+    AdministratorProfile,
+)
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin): 
 # Register your models here.
