8d90e6dbd0659450d3bfeac5963c1ce1ecae6196
kjohnbosco62-a11y <kjohnbosco62@gmail.com>
2026-04-25 20:32:02 +0000

    updates

--- frontend/aits_frontend/src/pages /student/StudentsResultsPage.jsx
+++ frontend/aits_frontend/src/pages /student/StudentsResultsPage.jsx
@@ -6,3 +6,7 @@ function StudentResultsPage() {
       <h1>Final Results</h1>
       <p>Your final evaluation scores and overall mark will appear here once assessments are completed and published.</p>
     </div>
+  );
+}
+
+export default StudentResultsPage;
