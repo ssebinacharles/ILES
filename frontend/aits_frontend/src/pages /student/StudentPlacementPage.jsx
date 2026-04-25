9ac610c50aac2776b2305f1b5cccbe87a3b63c60
kjohnbosco62-a11y <kjohnbosco62@gmail.com>
2026-04-25 20:16:13 +0000

    updates

--- frontend/aits_frontend/src/pages /student/StudentPlacementPage.jsx
+++ frontend/aits_frontend/src/pages /student/StudentPlacementPage.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 function StudentPlacementsPage() {
   return (
+     <div style={{ padding: '2rem' }}>
+      <h1>My Placements</h1>
+      <p>This page will display the list of your internship placements. You can create new placement requests or track existing ones here.</p>
+    </div>
